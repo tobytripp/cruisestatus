5bf68a71c246386c032bacbe5430a669e46f7c83
Toby <toby.tripp+git@gmail.com>
2010-01-23 14:25:10 -0600

    Docs.  Rename file to match gem name, D'oh.

---
+++ lib/cruisestatus.rb
@@ -0,0 +1,44 @@
+require 'rexml/document'
+require "open-uri"
+
+# Checks the status of one or more project builds on a cruisecontrol.rb
+# server.
+# 
+# == Example:
+# 
+#     if CruiseStatus.new( 'http://my.cruise.com/projects.rss' ).pass?
+#       puts "Build passed!"
+#     else
+#       puts "Build failed…Boo!"
+#     end
+#
+class CruiseStatus
+  
+  # feed_url::  URL pointing to a cruise.rb RSS feed.
+  #             Example: http://my.cruise.com/projects.rss
+  #                  or: http://my.cruise.com/projects/myproject.rss
+  # 
+  def initialize( feed_url )
+    project_feed = Kernel.open( feed_url ).read
+    @doc = REXML::Document.new project_feed
+  rescue Exception => e
+    @failures = [e.message]
+    @doc = REXML::Document.new ""
+  end
+ 
+  # True if all builds described by the feed are passing.
+  # 
+  def pass?
+    failures.empty?
+  end
+  
+  # A list of failing builds.  Empty if all builds passed.
+  # 
+  def failures
+    @failures ||= REXML::XPath.match( @doc, "//item/title" ).select { |element|
+      element.text =~ /failed$/
+    }.map do |element|
+      element.text.gsub( /(.*) build (.+) failed$/, '\1' )
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'cruise_status'
+require 'cruisestatus'
 
 require "rubygems"
 require 'spec'
