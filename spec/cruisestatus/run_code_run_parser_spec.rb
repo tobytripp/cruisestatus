03be65045d160d6c4b1d00ee7daa29710bd12f6a
Toby <toby.tripp+git@gmail.com>
2010-02-10 11:14:55 -0600

    Update runcoderun spec to include actual API output.  Fix intentionally failing spec

--- spec/cruisestatus/run_code_run_parser_spec.rb
+++ spec/cruisestatus/run_code_run_parser_spec.rb
@@ -22,7 +22,6 @@ describe CruiseStatus::RunCodeRunParser do
     end
     
     it "deliberately fail" do
-      fail "verify failure api output"
       @parser.failures.should == ["cruisestatus"]
     end
     
@@ -34,5 +33,5 @@ RCR_PASS_RESPONSE = <<-EOS
 EOS
 
 RCR_FAIL_RESPONSE = <<-EOS
-{"user":{"email":"toby.tripp+runcoderun.com@gmail.com","username":"tobytripp","projects":[{"ended_at":"2010-01-28T12:33:16-06:00","git_url":"http:\/\/github.com\/tobytripp\/cruisestatus","author_name":"Toby","url":"http:\/\/runcoderun.com\/tobytripp\/cruisestatus","branch":"refs\/heads\/master","commit":"7ced1af9afecbb6396b78f86341b4121f026248c","name":"cruisestatus","status":"fail","description":"Check the status of your cruise.rb build from your Ruby scripts.","build_url":"http:\/\/runcoderun.com\/tobytripp\/cruisestatus\/builds\/7ced1af9afecbb6396b78f86341b4121f026248c","commit_message":"Regenerated gemspec for version 1.2.1"}]}}
+{"user":{"email":"toby.tripp+runcoderun.com@gmail.com","username":"tobytripp","projects":[{"ended_at":"2010-02-10T11:13:16-06:00","git_url":"http:\/\/github.com\/tobytripp\/cruisestatus","author_name":"Toby","url":"http:\/\/runcoderun.com\/tobytripp\/cruisestatus","branch":"refs\/heads\/master","commit":"9a951f0fdbefcd9a019f799049ce4658e6e3686f","name":"cruisestatus","status":"failure","description":"Check the status of your cruise.rb build from your Ruby scripts.","build_url":"http:\/\/runcoderun.com\/tobytripp\/cruisestatus\/builds\/9a951f0fdbefcd9a019f799049ce4658e6e3686f","commit_message":"Fix constant-collision"}]}}
 EOS
